0c468a6237b5d44d617b4b451e4c60dd0ddfdf63
Muhammad Musa Mueed <musavuswh@gmail.com>
2024-12-24 23:51:14 +0500

    1,2) Getting Started & CSS Styling, (completed) example project dashboard setup, part-1

---
+++ Learn NextJS Docs/Chapter 1/pnpm as your Package Manager.js
@@ -0,0 +1,14 @@
+/*
+We recommend using pnpm as your package manager, as it is faster and more efficient than npm or yarn. If you don't have "pnpm" installed, you can install it globally by running:
+
+-> npm install -g pnpm
+
+More info about pnpm, go to this website "https://pnpm.io/"
+
+To create a Next.js app, open your terminal, "cd" into the folder you'd like to keep your project and run the following command:
+
+-> npx create-next-app@latest nextjs-dashboard --example "https://github.com/vercel/next-learn/tree/main/dashboard/starter-example" --use-pnpm
+
+This cmd uses "create-next-app", a CLI (Command Line Interface) tool that sets up a Next.js application for you.
+
+*/
